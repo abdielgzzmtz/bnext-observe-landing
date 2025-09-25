509f79ba9f541e4924ed57fafe2b4c1d3845e040
abdielgzzmtz <70164680+abdielgzzmtz@users.noreply.github.com>
2025-09-25 09:29:21 -0600

    Refactor email sending logic and update template

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -14,8 +14,7 @@ const transporter = nodemailer.createTransport({
     // no usar rejectUnauthorized: false en producción a menos que entiendas el riesgo.
     // ciphers: 'TLSv1.2' // opcional si tu servidor lo requiere
     
-    requireTLS: true,
-    tls: { minVersion: "TLSv1.2" },
+    ciphers: 'TLSv1.2' // opcional si tu servidor lo requiere
   },
 });
 
@@ -238,4 +237,5 @@ function getTemplate(name: string, company: string, email: string, phone: string
   `
 
 }
+
 
